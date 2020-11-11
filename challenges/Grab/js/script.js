52f719fc7161978bd9b34b2a6413fa692f39ac8e
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-11-11 02:17:56 -0300

    Adiciona validação do email

--- challenges/Grab/js/script.js
+++ challenges/Grab/js/script.js
@@ -1,3 +1,7 @@
+const emailInput = document.querySelector('#e-mail')
+const emailLabel = document.querySelector('label[for="e-mail"]')
+const emailFeedback = document.getElementsByClassName('result')[0]
+
 const passwordInput = document.querySelector('#password')
 const passwordLabel = document.querySelector('label[for="password"]')
 const passwordFeedback = document.getElementsByClassName('result')[1]
@@ -12,5 +16,19 @@ function verifyPasswordValue() {
     passwordInput.style.borderColor = "#3CDC8C"
     passwordLabel.style.color = "#3CDC8C"
     passwordFeedback.innerHTML = ''
+  }
+}
+function validateEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
+function verifyEmailValue() {
+  if (validateEmail(emailInput.value)) {
+    emailInput.style.borderColor = "#3CDC8C"
+    emailLabel.style.color = "#3CDC8C"   
+    emailFeedback.innerHTML = ''
+  } else {
+    emailInput.style.borderColor = "#FF5757"
+    emailLabel.style.color = "#FF5757"    
+    emailFeedback.innerHTML = 'Digite um email válido!'
   }
 }
