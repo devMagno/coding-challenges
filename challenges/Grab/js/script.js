a49ab8f5f313632e69349a1f444e1a1b99914c13
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-11-11 02:18:56 -0300

    Refatora validação do campo de senha

--- challenges/Grab/js/script.js
+++ challenges/Grab/js/script.js
@@ -7,15 +7,15 @@ const passwordLabel = document.querySelector('label[for="password"]')
 const passwordFeedback = document.getElementsByClassName('result')[1]
 
 function verifyPasswordValue() {
-  if (passwordInput.value.length < 8) {
-    passwordInput.style.borderColor = "#FF5757"
-    passwordLabel.style.color = "#FF5757"
-    passwordFeedback.innerHTML = 'A senha deve ter no mínimo <b>8</b> caracteres!'
-  }
-  else {
+  if (passwordInput.value.length >= 8) {
     passwordInput.style.borderColor = "#3CDC8C"
     passwordLabel.style.color = "#3CDC8C"
     passwordFeedback.innerHTML = ''
+  }
+  else {
+    passwordInput.style.borderColor = "#FF5757"
+    passwordLabel.style.color = "#FF5757"
+    passwordFeedback.innerHTML = 'A senha deve ter no mínimo <b>8</b> caracteres!'
   }
 }
 function validateEmail(email) {
