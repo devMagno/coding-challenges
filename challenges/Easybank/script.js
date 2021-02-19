e437b9ceb19afd3e741f336fcbf7c9cdf55d72e3
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-02-19 16:16:31 -0300

    Adiciona animação do overlay

--- challenges/Easybank/script.js
+++ challenges/Easybank/script.js
@@ -1,5 +1,16 @@
 const btnHamburguer = document.querySelector('#btnHamburger')
 const header = document.querySelector('header.header')
+const overlay = document.querySelector('div.overlay')
+
 btnHamburguer.addEventListener('click', () => {
-  header.classList.toggle('open')
+  if(header.classList.contains('open')) {
+    header.classList.remove('open')
+    overlay.classList.add('fade-out')
+    overlay.classList.remove('fade-in')
+  }
+  else {
+    header.classList.add('open')
+    overlay.classList.add('fade-in')
+    overlay.classList.remove('fade-out')
+  }
 })
